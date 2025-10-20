016679f56987ebd0631cf7f05b0989b69a9d7018
Jinghui Liang <3414467112@qq.com>
2025-10-20 10:54:38 +0000

    feat: 添加事件回调函数以处理网络事件并释放资源

--- src/test_thread_pool/hftp_server_cmd.cpp
+++ src/test_thread_pool/hftp_server_cmd.cpp
@@ -22,14 +22,36 @@ static void ReadCB(struct bufferevent* bev, void* ctx) {
          << cmd_task->socket_fd_ << ": " << buf << endl;
 
     //  测试代码, 测试退出命令
-    if (strstr(buf, "exit") != nullptr) {
-      cout << "HFTPServerCmd::ReadCB() Exiting on socket "
-           << cmd_task->socket_fd_ << endl;
-      bufferevent_free(bev);
-      delete cmd_task;
-      break;
-    }
+    // if (strstr(buf, "exit") != nullptr) {
+    //   cout << "HFTPServerCmd::ReadCB() Exiting on socket "
+    //        << cmd_task->socket_fd_ << endl;
+    //   bufferevent_free(bev);
+    //   delete cmd_task;
+    //   break;
+    // }
   }
+}
+
+static void EventCB(struct bufferevent* bev, short what, void* ctx) {
+  // 事件回调函数
+  HFTPServerCmd* cmd_task = static_cast<HFTPServerCmd*>(ctx);
+  // 如果对方网络断掉, 或者机器崩溃, 有可能不会收到 EOF (超时解决)
+  if (what & BEV_EVENT_EOF) {
+    cout << "HFTPServerCmd::EventCB() Connection closed on socket "
+         << cmd_task->socket_fd_ << endl;
+  } else if (what & BEV_EVENT_ERROR) {
+    cout << "HFTPServerCmd::EventCB() Error on socket " << cmd_task->socket_fd_
+         << endl;
+  } else if (what & BEV_EVENT_TIMEOUT) {
+    cout << "HFTPServerCmd::EventCB() Timeout on socket "
+         << cmd_task->socket_fd_ << endl;
+  } else {
+    cout << "HFTPServerCmd::EventCB() Unknown event on socket "
+         << cmd_task->socket_fd_ << endl;
+  }
+  // 释放资源
+  bufferevent_free(bev);
+  delete cmd_task;
 }
 
 // 初始化任务, 运行在子线程中
@@ -50,8 +72,13 @@ bool HFTPServerCmd::Init() {
   }
 
   // 设置读写回调
-  bufferevent_setcb(bev, ReadCB, 0, 0, this);
+  bufferevent_setcb(bev, ReadCB, nullptr, EventCB, this);
   bufferevent_enable(bev, EV_READ | EV_WRITE);
+
+  // 添加超时设置(读10秒)
+  timeval read_timeout = {10, 0};  // 10秒读超时(秒, 微秒)
+  timeval write_timeout = {0, 0};  // 不设置写超时
+  bufferevent_set_timeouts(bev, &read_timeout, &write_timeout);
 
   return true;
 }

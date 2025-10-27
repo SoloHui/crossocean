5ca7cafdb1364c255a0bd2ca7061642949c4fc81
Jinghui Liang <3414467112@qq.com>
2025-10-27 15:30:32 +0800

    feat: 添加hdisk_server应用程序及其CMake配置

---
+++ src/hdisk_server/hdisk_server.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <signal.h>
+#endif
+
+#include "hthread_pool.h"
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+#ifdef _WIN32
+  // 初始化`socket`
+  WSADATA wsaData;
+  WSAStartup(MAKEWORD(2, 2), &wsaData);
+#else
+  // 忽略`SIGPIPE`信号, 防止程序因向已关闭的socket写数据而终止
+  if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+    cerr << "main(): signal SIGPIPE error" << endl;
+    return -1;
+  }
+#endif
+
+  // 服务器端口号
+  int server_port = 9340;
+  // 线程池大小
+  int thread_num = 10;
+
+  if (argc > 1) {
+    server_port = atoi(argv[1]);
+  }
+  if (argc > 2) {
+    thread_num = atoi(argv[2]);
+  }
+  if (argc == 1) {
+    cout << "Usage: hdisk_server [server_port] [thread_num]" << endl;
+  }
+  cout << "Starting hdisk_server on port " << server_port << "..." << endl;
+  cout << "Using thread pool size: " << thread_num << endl;
+
+  // 初始化线程池
+  HThreadPool::GetInstance()->Init(thread_num);
+
+  return 0;
+}
